fdea30caf56194d1a9bd6181d4c6116306be337a
h-fukuyama <h-fukuyama@usen-next.jp>
2023-06-02 17:47:01 +0900

    「次へ」「前へ」ボタン追加

--- index.php
+++ index.php
@@ -1,29 +1,42 @@
 <?php
-$url = 'https://pokeapi.co/api/v2/pokemon/?limit=10&offset=0';
-$response = file_get_contents($url);
-$data = json_decode($response, true);
-
-print("<div class='parent'>");
-foreach($data['results'] as $key => $value){
-    $pokemon_url = $value['url'];
-    $pokemon_response = file_get_contents($pokemon_url);
-    $pokemon_data = json_decode($pokemon_response, true);
-
-    //var_dump($pokemon_data);
-    print("<div class='child'>");
-    echo "name: " . $pokemon_data['name'] . "<br>";
-    $picture = $pokemon_data['sprites']['front_default'];
-    echo "<img src=$picture><br>";
-    echo "type: ";
-    foreach($pokemon_data['types'] as $key => $val){
-        echo $val['type']['name'] . " ";
+function show(){
+    if(!isset($_GET['page_id'])){
+        $now = 0;
+    }else{
+        $now = $_GET['page_id'];
     }
-    echo "<br>";
-    echo "height: " . $pokemon_data['height'] . "<br>";
-    echo "weight: " . $pokemon_data['weight'];
+    $url = "https://pokeapi.co/api/v2/pokemon/?limit=10&offset=" . $now;
+    $response = file_get_contents($url);
+    $data = json_decode($response, true);
+
+    print("<div class='parent'>");
+    foreach($data['results'] as $key => $value){
+        $pokemon_url = $value['url'];
+        $pokemon_response = file_get_contents($pokemon_url);
+        $pokemon_data = json_decode($pokemon_response, true);
+
+        //var_dump($pokemon_data);
+        print("<div class='child'>");
+        echo "name: " . $pokemon_data['name'] . "<br>";
+        $picture = $pokemon_data['sprites']['front_default'];
+        echo "<img src=$picture><br>";
+        echo "type: ";
+        foreach($pokemon_data['types'] as $key => $val){
+            echo $val['type']['name'] . " ";
+        }
+        echo "<br>";
+        echo "height: " . $pokemon_data['height'] . "<br>";
+        echo "weight: " . $pokemon_data['weight'];
+        print("</div>");
+    }
     print("</div>");
 }
-print("</div>");
+
+if(!isset($_GET['page_id'])){
+    $now = 0;
+}else{
+    $now = $_GET['page_id'];
+}
 
 
 ?>
@@ -38,5 +51,13 @@ print("</div>");
     <title>Document</title>
 </head>
 <body>
+    <?php show() ?>
+    <form action="" method="get">
+    <input type="button" id="back" value="前へ">
+    </form>
+    <form action="" method="next">
+    <input type="button" id="back" value="次へ">
+    </form>
+
 </body>
 </html>
