05c426bba0a87101445e898826c9a80ac0f2eb23
h-fukuyama <h-fukuyama@usen-next.jp>
2023-06-07 17:40:44 +0900

    冗長な処理の関数化、ポケモンオブジェクト定義によってechoを簡略化

--- index.php
+++ index.php
@@ -1,14 +1,47 @@
 <?php
+###########################################
+#ポケモンオブジェクトの定義
+class Pokemon {
+    public $id;
+    public $name;
+    public $types;
+    public $height;
+    public $weight;
+    public $description;
 
+    public function __construct($id, $name, $types, $height, $weight, $description) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->types = $types;
+        $this->height = $height;
+        $this->weight = $weight;
+        $this->description = $description;
+    }
+}
+###########################################
+
+
+###########################################
+#指定したURLをjsonにデコードする関数
+function decode_url($url) {
+    $response = file_get_contents($url);
+    $data = json_decode($response, true);
+    return $data;
+}
+###########################################
+
+
+###########################################
+#リストを表示する関数
 function show(){
     session_start();
-
+    #リスト数、ページの頭の初期値を設定
     if(empty($_SESSION['list'])||empty($_SESSION['now'])){
         $_SESSION['list'] = 10;
         $_SESSION['now'] = 0;
     }
 
-    #listは件数
+    #リスト数の変更があった場合の設定（SESSIONで保持）
     if(isset($_GET['list'])){
         if($_GET['list']==10||empty($_SESSION['list'])){
             $_SESSION['list'] = 10;
@@ -22,7 +55,7 @@ function show(){
         }
     }
 
-    #nowは最初の
+    #ページの頭の数字を設定（SESSIONで保持）
     if(isset($_GET['page'])){
         if($_GET['page']=="next"){
             $_SESSION['now'] = $_SESSION['now'] + $_SESSION['list'];
@@ -33,57 +66,31 @@ function show(){
         }
     }
 
-    $url = "https://pokeapi.co/api/v2/pokemon/?limit=" . $_SESSION['list'] . "&offset=" . $_SESSION['now'];
-    $response = file_get_contents($url);
-    $data = json_decode($response, true);
+    #SESSION情報をもとに10匹分のポケモンを拾ってくる
+    $data = decode_url("https://pokeapi.co/api/v2/pokemon/?limit=" . $_SESSION['list'] . "&offset=" . $_SESSION['now']);
+
     echo "<div class='book'>";
-    foreach($data['results'] as $key => $value){
-        print("<div class='card' onclick='flipCard(this)'>");
+    foreach($data['results'] as $key => $value){ #ポケモン１匹ずつで表示の処理を回す
+        $types_text = "";
         $pokemon_url = $value['url'];
-        $pokemon_response = file_get_contents($pokemon_url);
-        $pokemon_data = json_decode($pokemon_response, true);
+        $pokemon_data = decode_url($pokemon_url);
+        $picture = $pokemon_data['sprites']['front_default'];#前面画像
+        $picture_back = $pokemon_data['sprites']['back_default'];#背面画像
+        $picture_official = $pokemon_data['sprites']['other']['official-artwork']['front_default'];#オフィシャルアートワーク
+        
+        #属性は複数あるパターンがあるのでforeachで配列に代入
+        foreach($pokemon_data['types'] as $key => $val){
+            $types_data = decode_url($val['type']['url']);
+            $types_text = $types_text . " " . $types_data['names'][0]['name'];
+        }
+
+        #高さ、重さを格納している別URLをデコード
         $uri = rtrim($pokemon_url, '/');
         $uri = substr($uri, strrpos($uri, '/') + 1);
         $url_species = "https://pokeapi.co/api/v2/pokemon-species/" . $uri;
-        $response_species = file_get_contents($url_species);
-        $species_data = json_decode($response_species, true);
+        $species_data = decode_url($url_species);
 
-        // var_dump($species_data['flavor_text_entries']['ja']['flavor_text']);
-        print("<div class='front'>");
-        echo "no. " . $pokemon_data['id'] . " <b>" . $species_data['names'][0]['name'] . "</b><br>";
-        $picture = $pokemon_data['sprites']['front_default'];
-        echo "<img src=$picture height=100px width=100px><br>";
-        echo "ぞくせい: ";
-        foreach($pokemon_data['types'] as $key => $val){
-            $types_url = $val['type']['url'];
-            $types_response = file_get_contents($types_url);
-            $types_data = json_decode($types_response, true);
-            echo $types_data['names'][0]['name'] . " ";
-        }
-        echo "<br>";
-        echo "たかさ: " . $pokemon_data['height']*10 . "cm<br>";
-        echo "おもさ: " . $pokemon_data['weight']/10 . "kg";
-        print("</div>");
-
-        print("<div class='back'>");
-        #ここからカードの裏側情報
-        $picture_official = $pokemon_data['sprites']['other']['official-artwork']['front_default'];
-        $picture_back = $pokemon_data['sprites']['back_default'];
-        echo "<div class='container'>";
-        echo "<img src=$picture_official class='left'>
-            <div id='right'>
-            <img src=$picture>
-            <img src=$picture_back><br>
-            </div></div>";
-        echo "no. " . $pokemon_data['id'] . " <b>" . $species_data['names'][0]['name'] . "</b>";
-        echo "ぞくせい: ";
-        foreach($pokemon_data['types'] as $key => $val){
-            $types_url = $val['type']['url'];
-            $types_response = file_get_contents($types_url);
-            $types_data = json_decode($types_response, true);
-            echo $types_data['names'][0]['name'] . " ";
-        }
-        echo "<br>";
+        #別URLから説明文もデコード（日本語のテキストを探す）
         $flavorTexts = $species_data['flavor_text_entries'];
         foreach ($flavorTexts as $flavorText) {
             if($flavorText['language']['name'] === 'ja-Hrkt'){
@@ -91,11 +98,43 @@ function show(){
                 break;
             }
         }
-        echo $japaneseDescription;
-        print("</div>");
-        print("</div>");
+
+        #最後にPokemonオブジェクトに値を指定
+        $pokemon = new Pokemon(
+            $pokemon_data['id'],
+            $species_data['names'][0]['name'],
+            $types_text,
+            $pokemon_data['height']*10,
+            $pokemon_data['weight']/10,
+            $japaneseDescription
+        );
+
+        #ブラウザ上に表示
+        echo <<< EOM
+        <div class='card' onclick='flipCard(this)'>
+            <div class='front'>
+                no.  {$pokemon->id} <b>{$pokemon->name}</b><br>
+                <img src={$picture} height=100px width=100px><br>
+                ぞくせい: {$pokemon->types}<br>
+                たかさ: {$pokemon->height}cm<br>
+                おもさ: {$pokemon->weight}kg
+            </div>
+            <div class='back'>
+                <div class='container'>
+                    <img src=$picture_official class='left'>
+                    <div id='right'>
+                        <img src=$picture>
+                        <img src=$picture_back>
+                    </div>
+                </div>
+                no.  {$pokemon->id} <b>{$pokemon->name}</b>
+                ぞくせい: {$pokemon->types}<br>
+                $japaneseDescription
+            </div>
+        </div>
+        EOM;
     }
-    echo "</div>";
+    print("</div>");
 }
 
 ?>
